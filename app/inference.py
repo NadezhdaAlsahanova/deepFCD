3eae7dbacf086fdb10888c44c38eeff11bb2c5f3
NadezhdaAlsahanova <72088271+NadezhdaAlsahanova@users.noreply.github.com>
2024-01-18 16:31:26 +0300

    Deleted useless raws and simpled inference.py

--- app/inference.py
+++ app/inference.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import logging
 import multiprocessing
 import os
@@ -39,7 +37,7 @@ except ValueError:
         "The environment variable OMP_NUM_THREADS"
         " should be a number, got '%s'." % var
     )
-# os.environ['openmp'] = 'True'
+
 os.environ["THEANO_FLAGS"] = f"mode=FAST_RUN,device=cpu,openmp=True,floatX=float32,compiledir={os.getpid()}"
 
 logging.info(os.environ["THEANO_FLAGS"])
@@ -53,140 +51,59 @@ from utils.metrics import *
 import multiprocessing
 
 
-def prediction_sub(intput_dir,output_dir):  
-    args.t1_fname = str([i for i in os.listdir(os.path.join(args.dir, args.id)) if re.findall(f'^{args.id}_.*_T1w.nii.gz', str(i))][0])
-    args.t2_fname = str([i for i in os.listdir(os.path.join(args.dir, args.id)) if re.findall(f'^{args.id}_.*_FLAIR.nii.gz', str(i))][0])
+def prediction_sub(model, args):  
+    args.t1_fname = f'{args.id}_t1_brain-final.nii.gz'
+    args.t2_fname = f'{args.id}_fl_brain-final.nii.gz'
+    
     if not os.path.isabs(args.dir):
         args.dir = os.path.abspath(args.dir)
     
-    
-    # co-register T1 and T2 images to MNI152 space and N3 correction before brain extraction (True/False)
-    args.outdir = os.path.join(output_dir, args.id)
-    
-    args.t1 = os.path.join(args.dir, args.id, args.t1_fname)
-    args.t2 = os.path.join(args.dir, args.id, args.t2_fname)
-    
-    args.t1_orig, args.t2_orig = args.t1, args.t2
-    
-    if bool(args.brain_masking):
-        args.use_gpu = False
-        # MRI pre-processing configuration
-        args.output_suffix = "_brain_final.nii.gz"
-    
-        preprocess_sh = os.path.join(cwd, "preprocess.sh")
-        subprocess.check_call(
-            [
-                preprocess_sh,
-                args.id,
-                args.t1,
-                args.t2,
-                output_dir,
-                bool2str(args.preprocess),
-                bool2str(args.use_gpu),
-            ]
-        )
-    
-        args.t1 = os.path.join(args.outdir, args.id + "_t1" + args.output_suffix)
-        args.t2 = os.path.join(args.outdir, args.id + "_t2" + args.output_suffix)
-    else:
-        logging.info(
-            "Skipping image preprocessing and brain masking, presumably images are co-registered, bias-corrected, and skull-stripped"
-        )
-    
-    if os.environ.get("CI_TESTING") is not None:
-        options["CI_TESTING_GT"] = os.environ.get("CI_TESTING_GT")
-        print("CI environment initialized: {}".format(options["CI_TESTING_GT"]))
-        mask = ants.image_read(options["CI_TESTING_GT"])
-        t1, t2 = ants.image_read(args.t1), ants.image_read(args.t2)
-        ants.mask_image(t1, mask, level=1, binarize=False).to_filename(args.t1)
-        ants.mask_image(t2, mask, level=1, binarize=False).to_filename(args.t2)
-    
-    
-    # --------------------------------------------------
-    # test the cascaded model
-    # --------------------------------------------------
+    args.mask_path = os.path.join(args.dir, args.id, f'{args.id}_exclusive_mask.nii.gz')
 
-    test_list = [args.id]
-    t1_file = args.t1
-    t2_file = args.t2
+    t1 = nib.load(os.path.join(args.dir, args.id, args.t1_fname)) 
+    t2 = nib.load(os.path.join(args.dir, args.id, args.t2_fname)) 
+    mask = nib.load(args.mask_path).get_fdata() < 0.5
     
-    t1_transform = os.path.join(
-        args.outdir, "transforms", args.id + "_t1-native-to-MNI152.mat"
-    )
-    t2_transform = os.path.join(
-        args.outdir, "transforms", args.id + "_t2-native-to-MNI152.mat"
-    )
-    
-    files = [args.t1, args.t2]
-    
-    orig_files = {"T1": args.t1_orig, "FLAIR": args.t2_orig}
-    
-    transform_files = [t1_transform, t2_transform]
-    # files = {}
-    # files['T1'], files['FLAIR'] = str(t1_file), t2_file
-    test_data = {}
-    # test_data = {f: {m: os.path.join(tfolder, f, m+'_stripped.nii.gz') for m in modalities} for f in test_list}
+    args.t1 = os.path.join(self.outdir, args.t1_fname)
+    args.t2 = os.path.join(self.outdir, args.t2_fname)
+
+    nib.save(nib.Nifti1Image(t1.get_fdata()*mask, t1.affine), args.t1)  
+    nib.save(nib.Nifti1Image(t2.get_fdata()*mask, t2.affine), args.t2) 
+
     test_data = {
-        f: {
-            m: os.path.join(options["test_folder"], f, n) for m, n in zip(modalities, files)
+        args.id: {
+            m: n for m, n in zip(modalities, [args.t1, args.t2])
         }
-        for f in test_list
     }
-    test_transforms = {
-        f: {m: n for m, n in zip(modalities, transform_files)} for f in test_list
-    }
-    # test_data = {f: {m: os.path.join(options['test_folder'], f, n) for m, n in zip(modalities, files)} for f in test_list}
-    for _, scan in enumerate(
-        tqdm(test_list, desc="serving predictions using the trained model", colour="blue")
-    ):
-        
-        if not os.path.exists(os.path.join(options["test_folder"], scan)):
-            os.mkdir(os.path.join(options["test_folder"], scan))
-        t_data = {}
-        t_data[scan] = test_data[scan]
-        transforms = {}
-        transforms[scan] = test_transforms[scan]
-    
-        options["pred_folder"] = os.path.join(
-            options["test_folder"], scan, options["experiment"]
-        )
-        if not os.path.exists(options["pred_folder"]):
-            os.mkdir(options["pred_folder"])
-    
-        pred_mean_fname = os.path.join(options["pred_folder"], scan + "_prob_mean_1.nii.gz")
-        pred_var_fname = os.path.join(options["pred_folder"], scan + "_prob_var_1.nii.gz")
-    
-        if np.logical_and(os.path.isfile(pred_mean_fname), os.path.isfile(pred_var_fname)):
-            logging.info("prediction for {} already exists".format(scan))
-            continue
-    
-        options["test_scan"] = scan
-    
-        start = time.time()
-        logging.info("\n")
-        logging.info("-" * 70)
-        logging.info("testing the model for scan: {}".format(scan))
-        logging.info("-" * 70)
-    
-        # if transform(s) do not exist (i.e., no preprocessing done), then skip (see base.py#L412)
-        if not any([os.path.exists(transforms[scan]["T1"]), os.path.exists(transforms[scan]["FLAIR"])]):
-            transforms = None
-    
-        test_model(
-            model,
-            t_data,
-            options,
-            transforms=transforms,
-            orig_files=orig_files,
-            invert_xfrm=True,
-            uncertainty=False,
-        )
-    
-        end = time.time()
-        diff = (end - start) // 60
-        logging.info("-" * 70)
-        logging.info("time elapsed: ~ {} minutes".format(diff))
-        logging.info("-" * 70)
+
+    options["pred_folder"] = args.outdir
+    options["test_folder"] = args.outdir
+    options["test_scan"] = args.id
+
+    start = time.time()
+    logging.info("\n")
+    logging.info("-" * 70)
+    logging.info("testing the model for scan: {}".format(scan))
+    logging.info("-" * 70)
+
+    test_model(
+        model,
+        test_data,
+        options,
+        uncertainty=False,
+    )
+
+    end = time.time()
+    diff = (end - start) // 60
+    logging.info("-" * 70)
+    logging.info("time elapsed: ~ {} minutes".format(diff))
+    logging.info("-" * 70)
+
+    os.remove(args.t1)
+    os.remove(args.t2)
+    os.remove(os.path.join(options["pred_folder"], scan + "_prob_mean_0.nii.gz"))
+
+
 
 
 if __name__ == '__main__':
@@ -195,9 +112,8 @@ if __name__ == '__main__':
     
     args = Data()
     args.dir = intput_dir
-    args.brain_masking = 0
-    args.preprocess = 0
     args.id = os.listdir(args.dir)[0]
+    args.outdir = output_dir #os.path.join(output_dir, args.id)
     
     cwd = os.path.realpath(os.path.dirname(__file__))
     # deepFCD configuration
@@ -208,14 +124,12 @@ if __name__ == '__main__':
     modalities = ["T1", "FLAIR"]
     x_names = options["x_names"]
     
-    # seed = options['seed']
     options["dropout_mc"] = True
     options["batch_size"] = 350000
     options["mini_batch_size"] = 2048
     options["load_checkpoint_1"] = True
     options["load_checkpoint_2"] = True
     
-    # trained model weights based on 148 histologically-verified FCD subjects
     options["test_folder"] = output_dir
     options["weight_paths"] = os.path.join(cwd, "weights")
     options["experiment"] = "noel_deepFCD_dropoutMC"
@@ -251,4 +165,4 @@ if __name__ == '__main__':
     model[1] = load_model(load_weights)
     logging.info(model[1].summary())
 
-    prediction_sub(intput_dir,output_dir)
+    prediction_sub(model, args)
