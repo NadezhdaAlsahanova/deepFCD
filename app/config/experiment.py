aedc937411ecaefa6116907fcef971b0c5e936e8
NadezhdaAlsahanova <72088271+NadezhdaAlsahanova@users.noreply.github.com>
2024-03-14 03:18:25 +0300

    Update experiment.py

--- app/config/experiment.py
+++ app/config/experiment.py
@@ -54,8 +54,8 @@ options['train_split'] = 0.25
 
 # maximum number of epochs used to train the model
 options['max_epochs'] = 60
-options['max_epochs_1'] = 60
-options['max_epochs_2'] = 100
+options['max_epochs_1'] = 30
+options['max_epochs_2'] = 50
 options['patience'] = 15
 # file paths to store the network parameter weights. These can be reused for posterior use.
 options['weight_paths'] = None
