1c8e254a3eceefcb229eac4263f0084bbe714fb7
NadezhdaAlsahanova <72088271+NadezhdaAlsahanova@users.noreply.github.com>
2024-03-21 14:52:05 +0300

    Update inference.py

--- app/inference.py
+++ app/inference.py
@@ -37,7 +37,18 @@ except ValueError:
         " should be a number, got '%s'." % var
     )
 # os.environ['openmp'] = 'True'
-os.environ["THEANO_FLAGS"] = "mode=FAST_RUN,device=cpu,openmp=True,floatX=float32"
+options['cuda'] = 'cuda0' # cpu, cuda, cuda0, cuda1, or cudaX: flag using gpu 1 or 2
+if options['cuda'].startswith('cuda1'):
+    os.environ["THEANO_FLAGS"] = "mode=FAST_RUN,device=cuda1,floatX=float32,dnn.enabled=False"
+elif options['cuda'].startswith('cpu'):
+    os.environ['OMP_NUM_THREADS'] = str(multiprocessing.cpu_count())
+    os.environ['MKL_NUM_THREADS'] = str(multiprocessing.cpu_count())
+    os.environ['GOTO_NUM_THREADS'] = str(multiprocessing.cpu_count())
+    os.environ['openmp'] = 'True'
+    os.environ["THEANO_FLAGS"] = "mode=FAST_RUN,device=cpu,openmp=True,floatX=float32"
+else:
+    os.environ["THEANO_FLAGS"] = "mode=FAST_RUN,device=cuda2,floatX=float32,dnn.enabled=False"
+print(os.environ["THEANO_FLAGS"])
 
 
 from keras import backend as K
