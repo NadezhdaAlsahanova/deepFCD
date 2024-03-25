5bd918a4d6498a8ba1f8a734d5e9d98b3576f2fc
NadezhdaAlsahanova <72088271+NadezhdaAlsahanova@users.noreply.github.com>
2024-03-25 22:05:09 +0300

    Update train.py

--- app/train.py
+++ app/train.py
@@ -261,8 +261,8 @@ if __name__ == '__main__':
     
     options["parallel_gpu"] = False
    
-    options["batch_size"] = 350000
-    options["mini_batch_size"] = 4096
+    options["batch_size"] = 250000
+    options["mini_batch_size"] = 1024
     options["main_dir"] = intput_dir
     options["out_dir"] = output_dir
     options["test_folder"] = output_dir
