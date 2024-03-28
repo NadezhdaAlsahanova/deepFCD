0c703ba15e955e71fa209bde61331a2326d1e4b5
NadezhdaAlsahanova <72088271+NadezhdaAlsahanova@users.noreply.github.com>
2024-03-28 16:58:10 +0300

    Update experiment.py

--- app/config/experiment.py
+++ app/config/experiment.py
@@ -41,7 +41,7 @@ else:
 options['exclude'] = ['.DS_Store', '._.DS_Store', '078', '095']
 
 # threshold to select voxels for training, discarding CSF and darker WM in FLAIR
-options['thr'] = 0.1
+options['thr'] = 0.25
 options['min_th'] = options['thr'] # z-scored [10%ile=0.15, 15%ile=0.28, 20%ile=0.38, 25%ile=0.46, 28%ile=0.5, 5%ile=-0.05, 0%ile=-6]
 options['th_dnn_train_2'] = 0.1 # probabilistic
 
@@ -54,7 +54,7 @@ options['train_split'] = 0.25
 
 # maximum number of epochs used to train the model
 options['max_epochs'] = 60
-options['max_epochs_1'] = 50
+options['max_epochs_1'] = 60
 options['max_epochs_2'] = 100
 options['patience'] = 15
 # file paths to store the network parameter weights. These can be reused for posterior use.
